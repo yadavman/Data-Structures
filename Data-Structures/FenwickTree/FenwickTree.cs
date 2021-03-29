acbdb448a75e4192967af69ef22049f9eeb62a75
Manish <Manish@LAPTOP-BDVGEQAL>
2021-03-29 22:21:52 +0530

    Fenwick Tree

---
+++ Data-Structures/FenwickTree/FenwickTree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FenwickTree
+{
+    public class FenwickTree
+    {
+        private long[] tree;
+
+        public FenwickTree(int sz)
+        {
+            tree = new long[sz+1];
+        }
+
+        //Input should be 1 based index
+        //constrcution of Binary Indexed Tree
+        public FenwickTree(long[] values)
+        {
+            
+            if (values == null)
+                throw new ArgumentNullException("Array cannot be empty");
+            tree = new long[values.Length];
+            values.CopyTo(tree,0);
+            for (int i = 0; i < tree.Length; i++)
+            {
+                int j = i + LSB(i);
+                if (j < tree.Length)
+                    tree[j] += tree[i];
+            }
+        }
+
+        private int LSB(int i)
+        {
+            return i & -i;
+            
+        }
+
+        public long PrefixSum(int i)
+        {
+            long sum = 0;
+            while (i != 0)
+            {
+                sum += tree[i];
+                i = i - LSB(i);
+            }
+            return sum;
+        }
+
+        //Return sum [i,j]
+        public long Sum(int i,int j)
+        {
+            if (j < i)
+                throw new ArgumentException("j cannot be less than i");
+            return PrefixSum(j) - PrefixSum(i-1);
+        }
+
+        //Add k to index i , 1 based
+        public void Add(int i,long k)
+        {
+            while (i < tree.Length)
+            {
+                tree[i] += k;
+                i += LSB(i);
+            }
+        }
+    }
+}
---
+++ Data-Structures/FenwickTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FenwickTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fenwick Tree!");
+            //1 based so 0th value does not get used 
+            FenwickTree ft = new FenwickTree(new long[] { 0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+            Console.WriteLine("Sum from 1 to 10 {0}",ft.Sum(1, 10)); //Sum from index 1 to 10 [1,10]
+            Console.WriteLine("Sum from 2 to 4 {0}", ft.Sum(2, 4));
+
+            //addint 100 to index and the Getting Sum from Index [1,5] should return 115
+            ft.Add(1, 100);
+            Console.WriteLine("Added 100 to index 1 ");
+            Console.WriteLine("Sum from 1 to 5 {0}", ft.Sum(1,5));
+            Console.Read();
+            //try using normal Addition and fenwich Tree Addition 
+        }
+    }
+}
